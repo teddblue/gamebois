107d75032f8505d4938a2aea71f66058d422f43d
teddblue <89668804+teddblue@users.noreply.github.com>
2021-10-25 17:58:13 -0400

    .

--- docs/loadgame.js
+++ docs/loadgame.js
@@ -3,5 +3,7 @@ myElem.onclick = function() {
 	//do stuff
 }
 function myFunction(){
-    //stuff
+    function myFunction() {
+        document.getElementById("demo").innerHTML = "Hello Dear Visitor!</br> We are happy that you've chosen our website to learn programming languages. We're sure you'll become one of the best programmers in your country. Good luck to you!";
+    }
 }
