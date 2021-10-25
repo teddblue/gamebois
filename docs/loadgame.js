396675c0f4409ebca05536cd9bc7de13788b916e
teddblue <89668804+teddblue@users.noreply.github.com>
2021-10-25 18:00:19 -0400

    Update loadgame.js

--- docs/loadgame.js
+++ docs/loadgame.js
@@ -3,7 +3,5 @@ myElem.onclick = function() {
 	//do stuff
 }
 function myFunction(){
-    function myFunction() {
-        document.getElementById("demo").innerHTML = "Hello Visitor!</br> We are happy that you've chosen our website to learn programming languages. We're sure you'll become one of the best programmers in your country. Good luck to you!";
-    }
+    document.getElementById("demo").innerHTML = "Hello Dear Visitor!</br> We are happy that you've chosen our website to learn programming languages. We're sure you'll become one of the best programmers in your country. Good luck to you!";
 }
