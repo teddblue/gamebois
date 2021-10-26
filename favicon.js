045956ccbc92119f0620dcaadd0b435c34124890
teddblue <89668804+teddblue@users.noreply.github.com>
2021-10-26 18:22:13 -0400

    Update favicon.js

--- favicon.js
+++ favicon.js
@@ -1,13 +1,13 @@
 let favicon = document.querySelector("[rel=icon]")
 const title = new URLSearchParams(window.location.search).get('title')
-const type = new URLSearchParams(window.location.search).get('type')
+const console = new URLSearchParams(window.location.search).get('type')
 
 /*url = "/gamebois/imageAssets/" + type + "/" + title + ".png";*/
 /*games list*/
 const gb = ['tetris', 'pokemon-red', 'pokemon-blue']
 
 var url = "/gamebois/imageAssets/gameboislogo512.gif";
-if (type === 'gb') {
+if (console === 'gb') {
     if (gb.includes(String(game))) {
         url = "/gamebois/imageAssets" + "/gb/" + title + ".png";
     }
